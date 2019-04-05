6f3f9726e5f6e3bfd99e928f746febd19eb63db2
Jack Coleman <jack@autostructure.io>
2019-04-05 17:11:33 -0400

    first commit for certificate_age custom fact

---
+++ lib/facter/certificate_age.rb
@@ -0,0 +1,13 @@
+# This fact returns a hash based on the age of the agents SSL certificate
+Facter.add('certificate_age') do
+  setcode do
+    creation_time = File.birthtime("#{Facter.value(:puppet_ssldir)}/certs/#{Facter.value(:certname)}.pem")
+    # Return a hash { day: x, month: x, year: x }
+    time_hash = {
+      year: creation_time.year,
+      month: creation_time.month,
+      day: creation_time.day,
+    }
+    time_hash
+  end
+end
