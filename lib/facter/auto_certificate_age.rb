ffc2557498ef73dd6f3c57d2d3a23884c13794f6
Jack Coleman <jack@autostructure.io>
2019-04-11 15:27:40 -0400

    update certificate_age fact to create a facts.d file

---
+++ lib/facter/auto_certificate_age.rb
@@ -0,0 +1,20 @@
+require 'yaml/store'
+
+# This fact returns a hash based on the age of the agents SSL certificate
+Facter.add('auto_certificate_age') do
+  setcode do
+    facter_dir = Facter.value(:puppet_confdir).split('/')[0..-3].join('/') + '/facter/facts.d'
+    unless File.exist?("#{facter_dir}/birthday.yaml")
+      creation_time = File.ctime("#{Facter.value(:puppet_ssldir)}/certs/#{Facter.value(:fqdn)}.pem")
+      time_hash = {
+        year: creation_time.year,
+        month: creation_time.month,
+        day: creation_time.day,
+      }
+      fact_file = YAML::Store.new("#{facter_dir}/birthday.yaml")
+      fact_file.transaction do
+        fact_file['certificate_age'] = time_hash
+      end
+    end
+  end
+end
